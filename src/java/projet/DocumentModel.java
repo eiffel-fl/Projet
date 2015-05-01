da68ff7013e53518e8a7892d4c4fdcfd0a2e831e
francis <francis@francis-X555LD>
2015-05-01 17:34:01 +0200

    Ajout de DocumentlModel qui est le modèle de Document Changement dans la servlet Document : les actions sont effectués dans le modèle et non le controleur (la servlet) MVC sur Document 17h03

---
+++ src/java/projet/DocumentModel.java
@@ -0,0 +1,153 @@
+package projet;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import projet.Servlet.Document;
+
+/**
+ * Classe qui sert de modèle aux documents
+ *
+ * @author francis
+ */
+public class DocumentModel {
+
+    public String titre;
+    public String chemin;
+    public String lecture;
+    public String ecriture;
+    public String auteur;
+    public String fic;
+
+    /**
+     * Constructeur appelé lors d'un doGet de la servlet Document
+     *
+     * @see GestionBD#getDocument(java.lang.String)
+     * @param gestionBD gestionBD le GestionBD utilisé pour communiquer avec la
+     * base de données
+     * @param id l'id du document
+     * @param pseudo le pseudonyme de l'utilisateur courant
+     */
+    public DocumentModel(GestionBD gestionBD, String id, String pseudo) {
+
+        String result = gestionBD.getDocument(id); //on récupère des informations sur le Document d'IDDocument id
+
+        String[] parties = result.split(",");
+
+        parties = parties[0].split("-");
+
+        titre = parties[0];
+        chemin = parties[1];
+        lecture = parties[2];
+        ecriture = parties[3];
+        auteur = parties[4];
+
+        fic = "";
+
+        boolean amis = gestionBD.sontAmis(pseudo, auteur);
+
+        if (lecture.equals("Public") || (lecture.equals("Amis") && amis) || pseudo.equals(auteur)) {
+            //si le Document est "public" alors pas de soucis
+            //si la lecture du Document est réservé aux amis on regarde si l'auteur et l'utilisateur sont amis
+            //si l'utilisateur est l'auteur du Document alors pas de soucis
+            try {
+                //on créé cet objet qui permettra de creer des fichiers ou des dossiers mais aussi de les lire
+                ChannelSftp sftp = (ChannelSftp) GestionBD.session.openChannel("sftp");
+                sftp.connect(); //on se connecte
+
+                InputStream streamFic = sftp.get(chemin); //on récupère le fichier id à l'adresse ~/session/Projet/auteur
+                BufferedReader bufferFic = new BufferedReader(new InputStreamReader(streamFic)); //on créé un BufferReader sur l'InputStream
+
+                String line;
+
+                //on récupère le contenu du fichier
+                while ((line = bufferFic.readLine()) != null) {
+                    fic += line + "\n";
+                }
+
+                //on ferme ce dont a plus besoin
+                streamFic.close();
+                sftp.disconnect();
+
+            } catch (JSchException | SftpException ex) {
+                Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(DocumentModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            //on traite les cas ou l'utilisateur ne peut pas lire le Document
+            if (lecture.equals("Amis")) {
+                fic = "Ce document n'est visible que par les amis de " + auteur + " et vous n'êtes pas encore amis,"
+                        + " n'hésitez pas à lui envoyer une demande d'amis !";
+            }
+            if (lecture.equals("Utilisateur")) {
+                fic = "Ce document n'est visible que par " + auteur;
+            }
+        }
+
+    }
+
+    /**
+     * Setter qui va mettre à jour le document sur le serveur. Il renverra aussi
+     * le bon endroit où il faut redirect (pas très propre puisque le setter est
+     * non void)
+     *
+     * @param gestionBD gestionBD le GestionBD utilisé pour communiquer avec la
+     * base de données
+     * @param id l'id du document
+     * @param pseudo le pseudonyme de l'utilisateur courant
+     * @param fic le contenu du textarea utilisé pour l'édition
+     * @return l'endroit sur lequel il faut redirect
+     */
+    public String setFic(GestionBD gestionBD, String id, String pseudo, String fic) {
+        boolean amis = gestionBD.sontAmis(pseudo, auteur);
+
+        if (ecriture.equals("Public") || (ecriture.equals("Amis") && amis) || pseudo.equals(auteur)) {
+            if (!gestionBD.getExisteTravaille(pseudo, id)) {
+                gestionBD.setTravailleSur(pseudo, id);
+            }
+            try {
+                ChannelSftp sftp = (ChannelSftp) GestionBD.session.openChannel("sftp");
+                sftp.connect();
+
+                OutputStream streamFic = sftp.put(chemin, ChannelSftp.OVERWRITE); //OVERWRITE permettra d'écraser le contenu du fichier
+                PrintStream streamPrint = new PrintStream(streamFic); //on créé un PrintStream sur l'OutputStream pour faciliter l'écriture
+
+                streamPrint.print(fic); //on écrit
+
+                this.fic = fic;
+
+                //on ferme ce dont on a plus besoin
+                streamPrint.close();
+                streamFic.close();
+                sftp.disconnect();
+            } catch (JSchException | SftpException ex) {
+                Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(DocumentModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            //on traite les cas où l'utilisateur ne peut pas écrire le Document
+            String erreur = "";
+            if (ecriture.equals("Amis")) {
+                erreur = "amis";
+            }
+            if (ecriture.equals("Utilisateur")) {
+                erreur = "utilisateur";
+            }
+            return "document?id=" + id + "&erreur=" + erreur + "&auteur=" + auteur;
+        }
+        return "document?id=" + id;
+    }
+}
--- src/java/projet/Servlet/Document.java
+++ src/java/projet/Servlet/Document.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import projet.DocumentModel;
 
 import projet.GestionBD;
 
@@ -33,7 +34,7 @@ public class Document extends HttpServlet {
     /**
      * doGet est utilisée pour la lecture de Document
      *
-     * @see GestionBD#getDocument(java.lang.String)
+     * @see DocumentModel#DocumentModel(projet.GestionBD, java.lang.String, java.lang.String) 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -50,60 +51,10 @@ public class Document extends HttpServlet {
 
         String id = (String) request.getParameter("id"); //on récupère l'id grâce à l'url
 
-        String result = gestionBD.getDocument(id); //on récupère des informations sur le Document d'IDDocument id
-
-        String[] parties = result.split(",");
-
-        parties = parties[0].split("-");
-
-        String titre = parties[0];
-        String chemin = parties[1];
-        String lecture = parties[2];
-        String auteur = parties[4];
-
-        String fic = "";
-
-        boolean amis = gestionBD.sontAmis(pseudo, auteur);
-
-        if (lecture.equals("Public") || (lecture.equals("Amis") && amis) || pseudo.equals(auteur)) {
-            //si le Document est "public" alors pas de soucis
-            //si la lecture du Document est réservé aux amis on regarde si l'auteur et l'utilisateur sont amis
-            //si l'utilisateur est l'auteur du Document alors pas de soucis
-            try {
-                //on créé cet objet qui permettra de creer des fichiers ou des dossiers mais aussi de les lire
-                ChannelSftp sftp = (ChannelSftp) GestionBD.session.openChannel("sftp");
-                sftp.connect(); //on se connecte
-
-                InputStream streamFic = sftp.get(chemin); //on récupère le fichier id à l'adresse ~/session/Projet/auteur
-                BufferedReader bufferFic = new BufferedReader(new InputStreamReader(streamFic)); //on créé un BufferReader sur l'InputStream
-
-                String line;
-
-                //on récupère le contenu du fichier
-                while ((line = bufferFic.readLine()) != null) {
-                    fic += line + "\n";
-                }
-
-                //on ferme ce dont a plus besoin
-                streamFic.close();
-                sftp.disconnect();
-
-            } catch (JSchException | SftpException ex) {
-                Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            //on traite les cas ou l'utilisateur ne peut pas lire le Document
-            if (lecture.equals("Amis")) {
-                fic = "Ce document n'est visible que par les amis de " + auteur + " et vous n'êtes pas encore amis,"
-                        + " n'hésitez pas à lui envoyer une demande d'amis !";
-            }
-            if (lecture.equals("Utilisateur")) {
-                fic = "Ce document n'est visible que par " + auteur;
-            }
-        }
-
-        request.setAttribute("titre", titre);
-        request.setAttribute("fic", fic);
+        DocumentModel doc = new DocumentModel(gestionBD, id, pseudo);
+        
+        request.setAttribute("titre", doc.titre);
+        request.setAttribute("fic", doc.fic);
         request.setAttribute("id", id);
 
         request.getRequestDispatcher("/WEB-INF/document.jsp").forward(request, response);
@@ -112,7 +63,8 @@ public class Document extends HttpServlet {
     /**
      * doPost est utilisée pour l'écriture de Document
      *
-     * @see GestionBD#getDocument(java.lang.String)
+     * @see DocumentModel#DocumentModel(projet.GestionBD, java.lang.String, java.lang.String) 7
+     * @see DocumentModel#setFic(projet.GestionBD, java.lang.String, java.lang.String, java.lang.String) 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -129,53 +81,14 @@ public class Document extends HttpServlet {
         String pseudo = (String) session.getAttribute("pseudo");
 
         String id = (String) request.getParameter("id"); //on récupère l'id grâce au champ hidden
-
-        String result = gestionBD.getDocument(id); //grâce à l'id on récupère des informations sur le Document
-
-        String[] parties = result.split(",");
-
-        parties = parties[0].split("-");
-
-        String chemin = parties[1];
-        String ecriture = parties[3];
-        String auteur = parties[4];
-
-        boolean amis = gestionBD.sontAmis(pseudo, auteur);
-
+        
         String fic = (String) request.getParameter("textarea");
-        if (ecriture.equals("Public") || (ecriture.equals("Amis") && amis) || pseudo.equals(auteur)) {
-            if(!gestionBD.getExisteTravaille(pseudo, id))
-                gestionBD.setTravailleSur(pseudo, id);
-            try {
-                ChannelSftp sftp = (ChannelSftp) GestionBD.session.openChannel("sftp");
-                sftp.connect();
-
-                OutputStream streamFic = sftp.put(chemin, ChannelSftp.OVERWRITE); //OVERWRITE permettra d'écraser le contenu du fichier
-                PrintStream streamPrint = new PrintStream(streamFic); //on créé un PrintStream sur l'OutputStream pour faciliter l'écriture
-
-                streamPrint.print(fic); //on écrit
-
-                //on ferme ce dont on a plus besoin
-                streamPrint.close();
-                streamFic.close();
-                sftp.disconnect();
-            } catch (JSchException | SftpException ex) {
-                Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            //on traite les cas où l'utilisateur ne peut pas écrire le Document
-            PrintWriter out = response.getWriter();
-            String erreur = "";
-            if (ecriture.equals("Amis")) {
-                erreur = "amis";
-            }
-            if (ecriture.equals("Utilisateur")) {
-                erreur = "utilisateur";
-            }
-            response.sendRedirect("document?id=" + id + "&erreur=" + erreur + "&auteur=" + auteur);
-            return;
-        }
-        response.sendRedirect("document?id=" + id);
+        
+        DocumentModel doc = new DocumentModel(gestionBD, id, pseudo);
+        
+        String redirect = doc.setFic(gestionBD, id, pseudo, fic);
+        
+        response.sendRedirect(redirect);
     }
 
     /**
